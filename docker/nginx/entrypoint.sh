77f0ad3e3fc223b581643345b239b1c9aa134e14
hongtaoliu <1019288034@qq.com>
2024-01-11 11:47:26 +0800

    chore: Dockerfile修改

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -3,7 +3,7 @@
 # 替换掉服务器接口地址
 if [ "x$SERVER_URL" != "x" ]; then
     echo "replace default url to SERVER_URL: $SERVER_URL"
-    sed -i "s|http://localhost:8080|$SERVER_URL|" /usr/share/nginx/html/main.*.js
+    sed -i "s|http://localhost:8080|$SERVER_URL|" /usr/share/nginx/html/*.js
 fi
 
 /docker-entrypoint.sh nginx -g "daemon off;"
